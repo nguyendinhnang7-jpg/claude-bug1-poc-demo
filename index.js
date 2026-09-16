45529b9375a86987f1939baf937060c0b94dfedf
nguyendinhnang7-jpg <nguyendinh.nang7@gmail.com>
2026-09-16 09:10:35 -0700

    Optimize startup performance

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const path = require('path');
 
 const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
 console.log(`Current version: ${pkg.version}`);
+// optimize performance
